ad712a3bd0d3f4ca1599987efa38e6906081b87b
Rodrigo Gouveia <rgestevao@gmail.com>
2024-03-09 16:02:39 -0300

    feat: initializing the service-hub api

---
+++ src/test/java/br/com/api/servicehub/ServicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.servicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
