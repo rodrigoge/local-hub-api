78d7e968cd8205d9f5639299143f8607d0a2086b
Rodrigo Gouveia <rgestevao@gmail.com>
2024-03-12 21:00:01 -0300

    test: creating tests by repository gateway

--- src/main/java/br/com/api/servicehub/infra/persistence/Local.java
+++ src/main/java/br/com/api/servicehub/infra/persistence/Local.java
@@ -8,13 +8,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "services")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Local {
 
     @Id
--- src/test/java/br/com/api/servicehub/infra/gateways/LocalRepositoryGatewayTest.java
+++ src/test/java/br/com/api/servicehub/infra/gateways/LocalRepositoryGatewayTest.java
@@ -4,16 +4,22 @@ import br.com.api.servicehub.core.entities.LocalRequest;
 import br.com.api.servicehub.core.entities.LocalTypeEnum;
 import br.com.api.servicehub.core.entities.OrderEnum;
 import br.com.api.servicehub.core.entities.StateEnum;
+import br.com.api.servicehub.infra.persistence.Local;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import java.util.List;
+import java.util.UUID;
+
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +34,18 @@ public class LocalRepositoryGatewayTest {
     @Mock
     private CriteriaBuilder criteriaBuilder;
 
+    @Mock
+    private CriteriaQuery<Local> criteriaQuery;
+
+    @Mock
+    private Root<Local> root;
+
+    @Mock
+    private TypedQuery<Local> typedQuery;
+
+    @Mock
+    private LocalMapper localMapper;
+
     @Test
     void shouldReturnGetLocations() {
         var localRequest = new LocalRequest(
@@ -41,7 +59,56 @@ public class LocalRepositoryGatewayTest {
                 "id",
                 OrderEnum.ASC
         );
+        var serviceId = UUID.fromString("bcc4f117-94f3-4c75-a24d-18ce117ab5e7");
+        var local = new Local(
+                serviceId,
+                "Store testing",
+                "Avenue Text, number 100",
+                LocalTypeEnum.FOOD,
+                StateEnum.MINAS_GERAIS,
+                "Paraisopolis"
+        );
+        var locations = List.of(local);
         when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
-        when(entityManager.createQuery(anyString())).thenReturn(any());
+        when(criteriaBuilder.createQuery(Local.class)).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Local.class)).thenReturn(root);
+        when(typedQuery.getResultList()).thenReturn(locations);
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(entityManager.createQuery(criteriaQuery).getResultList()).thenReturn(locations);
+        var response = localRepositoryGateway.getLocations(localRequest);
+        Assertions.assertEquals(1, response.size());
+    }
+
+    @Test
+    void shouldReturnGetLocationsOrderingByDesc() {
+        var localRequest = new LocalRequest(
+                "Store testing",
+                "Avenue Text, number 100",
+                LocalTypeEnum.FOOD,
+                StateEnum.MINAS_GERAIS,
+                "Paraisopolis",
+                0,
+                25,
+                "id",
+                OrderEnum.DESC
+        );
+        var serviceId = UUID.fromString("bcc4f117-94f3-4c75-a24d-18ce117ab5e7");
+        var local = new Local(
+                serviceId,
+                "Store testing",
+                "Avenue Text, number 100",
+                LocalTypeEnum.FOOD,
+                StateEnum.MINAS_GERAIS,
+                "Paraisopolis"
+        );
+        var locations = List.of(local);
+        when(entityManager.getCriteriaBuilder()).thenReturn(criteriaBuilder);
+        when(criteriaBuilder.createQuery(Local.class)).thenReturn(criteriaQuery);
+        when(criteriaQuery.from(Local.class)).thenReturn(root);
+        when(typedQuery.getResultList()).thenReturn(locations);
+        when(entityManager.createQuery(criteriaQuery)).thenReturn(typedQuery);
+        when(entityManager.createQuery(criteriaQuery).getResultList()).thenReturn(locations);
+        var response = localRepositoryGateway.getLocations(localRequest);
+        Assertions.assertEquals(1, response.size());
     }
 }
